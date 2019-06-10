59d7ef41979040d29b75302ba095313491feacb4
刘杰 <garila@qq.com>
2019-06-10 17:55:56 +0800

    ...add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbDatepickerModule, NbInputModule } from '@nebular/theme';
+import en from '@angular/common/locales/en';
+import zh from '@angular/common/locales/zh';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(zh);
+
 
 @NgModule({
   declarations: [
@@ -10,9 +17,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbDatepickerModule,
+    NbInputModule,
+    NbLayoutModule
   ],
-  providers: [],
+  providers: [
+    ...NbDatepickerModule.forRoot().providers,
+    { provide: LOCALE_ID, useValue: 'zh-CN' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
